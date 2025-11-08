54889d03d99d25bfc3c75da68c27927e9b3988dc
euEndro <andrew.doni.assis@gmail.com>
2025-11-08 10:41:05 -0300

    added user service and re

---
+++ backend/src/main/java/group/dl/backend/domain/user/UserService.java
@@ -0,0 +1,13 @@
+package group.dl.backend.domain.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+}
